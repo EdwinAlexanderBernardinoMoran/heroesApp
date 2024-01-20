c6dde4792684617558a937d1dbbb1384832abce5
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-01-19 22:21:00 -0600

    diseño de Sidenav, toolbar e icons

---
+++ src/assets/js/app.js
@@ -0,0 +1,14 @@
+$( document ).ready(function(){
+  $('.tabs').tabs();
+  $('select').formSelect();
+  $(".dropdown-trigger").dropdown();
+});
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const sidenav = document.querySelectorAll(".sidenav");
+//   const sidenavInstance = M.Sidenav.init(sidenav);
+
+//   const collapsible = document.querySelectorAll(".collapsible");
+//   const collapsibleInstance = M.Collapsible.init(collapsible);
+// })
