0a3c5231f1a05ab6df88a89b435af9a0aeab1592
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-07-27 20:13:59 -0600

    Guards nueva forma en Angular 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +7,4 @@ import { AuthService } from './auth/services/auth.service';
 })
 export class AppComponent {
   title = 'Heroes App';
-
-  constructor(private authService: AuthService){}
-
-  ngOnInit(): void{
-    this.authService.checkAuthentication().subscribe(
-      () => {
-        console.log('checkauthentication finished');
-      }
-    )
-  }
 }
